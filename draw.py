daeb2e37699297ce7afeb135363713c9f6522b8a
horazio <hfusco@stuy.edu>
2019-02-10 22:04:05 -0500

    function complete

--- draw.py
+++ draw.py
@@ -4,86 +4,56 @@ def draw_line(x0, y0, x1, y1, screen, color):
     if(x1 < x0):
         x1, x0 = x0, x1
         y1, y0 = y0, y1
-
-    x = x0
-    y = y0
-
-    A = y1 - y0
-    B = x0 - x1
-
-
     if(y1 - y0 > 0):
         if(x1 - x0 > y1 - y0):
-            line( A, B, x, y, x1, y1, screen, color, 1, 1)
+            oct = 2
         else:
-            line_vert(x0, y0, x1, y1, screen, color)
+            oct = 1
     else:
         if(x1 - x0 > y0 - y1):
-            down(x0, y0, x1, y1, screen, color)
+            oct = 3
         else:
-            down_vert(x0, y0, x1, y1, screen, color)
-            pass
+            oct = 4
 
-
-def line(A, B, x, y, x1, y1, screen, color, step0, step1, dmult):
-    d = 2 * A + B
-    while( x < x1):
-        plot(screen, color, x,y)
-        if(d * dmult > 0):
-            y += step0
-            d += 2 * B * step0
-        x += step1
-        d += 2 * A * step1
-
-        
-def line_vert( x0, y0, x1, y1, screen, color ):
-
+    line( x0, y0, x1, y1, screen, color, oct)
+    
+    
+    
+def line(x0, y0, x1, y1, screen, color, oct):
     x = x0
     y = y0
 
     A = y1 - y0
     B = x0 - x1
-
-    d = 2 * B + A
-
-    while( y < y1):
+    
+    if(oct == 2 or oct == 3):
+        d = 2 * A + B
+    else:
+        d = 2 * B + A
+        
+    while( x < x1):
         plot(screen, color, x,y)
-        if(d < 0):
+        if(oct == 1):
+            if(d < 0):
+                x += 1
+                d += 2 * A
+            y += 1
+            d += 2 * B
+        if(oct == 2):
+            if(d > 0):
+                y += 1
+                d += 2 * B
             x += 1
             d += 2 * A
-        y += 1
-        d += 2 * B
-
-def down(x0, y0, x1, y1, screen, color):
-
-    x = x0
-    y = y0
-
-    A = y1 - y0
-    B = x0 - x1
-    d = 2 * A + B
-
-    while( x < x1):
-        plot(screen, color, x,y)
-        if(d < 0):
+        if(oct == 3):
+            if(d < 0):
+                y -= 1
+                d -= 2 * B
+            x += 1
+            d += 2 * A
+        if(oct == 4):
+            if(d > 0):
+                x += 1
+                d += 2 * A
             y -= 1
             d -= 2 * B
-        x += 1
-        d += 2 * A
-
-
-def down_vert(x0, y0, x1, y1, screen, color):
-    x = x0
-    y = y0
-
-    A = y1 - y0
-    B = x0 - x1
-    d = 2 * B + A
-
-    while( x < x1):
-        plot(screen, color, x,y)
-        if(d > 0):
-            x += 1
-            d += 2 * A
-        y -= 1
-        d -= 2 * B
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from draw import *
 screen = new_screen()
 color = [ 0, 255, 0 ]
 
-draw_line(0,0, 100, 10, screen, color)
+draw_line(0,50, 500, 0, screen, color)
 
 display(screen)
 save_extension(screen, 'img.png')

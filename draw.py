9ee2cc9d542d32b10ef689e46fe9fb6b4bf11e16
horazio <hfusco@stuy.edu>
2019-02-11 21:41:06 -0500

    vertical lines now work

--- draw.py
+++ draw.py
@@ -7,13 +7,16 @@ def draw_line(x0, y0, x1, y1, screen, color):
     if(y1 - y0 > 0):
         if(x1 - x0 > y1 - y0):
             oct = 2
+          
         else:
             oct = 1
+          
     else:
         if(x1 - x0 > y0 - y1):
             oct = 3
+           
         else:
-            print("ppf\n")
+           
             oct = 4
 
     line( x0, y0, x1, y1, screen, color, oct)
@@ -31,28 +34,36 @@ def line(x0, y0, x1, y1, screen, color, oct):
         d = 2 * A + B
     else:
         d = 2 * B + A
-
-    while( x < x1):
-        plot(screen, color, x,y)
-        if(oct == 1):
+        
+        
+    
+    if(oct == 1):
+        while(y < y1):
+            plot(screen, color, x,y)
             if(d < 0):
                 x += 1
                 d += 2 * A
             y += 1
             d += 2 * B
-        if(oct == 2):
+    if(oct == 2):
+        while(x < x1):
+            plot(screen, color, x,y)
             if(d > 0):
                 y += 1
                 d += 2 * B
             x += 1
             d += 2 * A
-        if(oct == 3):
+    if(oct == 3):
+        while( x < x1):
+            plot(screen, color, x,y)
             if(d < 0):
                 y -= 1
                 d -= 2 * B
             x += 1
             d += 2 * A
-        if(oct == 4):
+    if(oct == 4):
+        while( y > y1):
+            plot(screen, color, x,y)
             if(d > 0):
                 x += 1
                 d += 2 * A
--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from draw import *
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
-draw_line(50, 0, 50, 100, screen, color)
-draw_line(0, 50, 100, 50, screen, color)
-'''
-i = 0
-j = 500
-while(i < 500):
-    draw_line(i, 0, 0, j, screen, [i, j, 200])
-    draw_line(i, j, 500, 500, screen, [i, j, 200])
-    draw_line(i, j, 150, 150, screen, [i, j, 200])
-    draw_line(i, j, 350, 350, screen, [i, j, 200])
-    i += 10
-    j -= 10
 
-'''
+x = -50;
+while(x < 500):
+    i = 0
+    j = 500
+    while(i < 500):
+        draw_line(i + x, 0 + x, 0 + x, j + x, screen, [i, j, 200])
+        #draw_line(i + x, j + x, 500 + x, 500 + x, screen, [i, j, 200])
+        #draw_line(i + x, j + x, 150 + x, 150 + x, screen, [i, j, 200])
+        #draw_line(i + x, j + x, 350 + x, 350 + x, screen, [i, j, 200])
+        i += 10
+        j -= 10
+    x += 50
+
 
 display(screen)
 save_extension(screen, 'img.png')
